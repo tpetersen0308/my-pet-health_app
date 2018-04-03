6cc674d595f6454a9465ab6d98f7656a67e569a2
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-03 15:47:23 -0700

    add current? method

--- app/models/health_screening.rb
+++ app/models/health_screening.rb
@@ -1,3 +1,12 @@
 class HealthScreening < ApplicationRecord
   belongs_to :pet
+
+  def current?
+    now = DateTime.now
+    if renewal_interval
+      (now.year * 12 + now.month) - (last_updated.year * 12 + last_updated.month) > 0
+    else
+      true
+    end
+  end
 end
