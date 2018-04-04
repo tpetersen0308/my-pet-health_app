23aa76deef81dd290a9cdf02e63d92b7a7725e33
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-04 12:00:32 -0700

    add screenings generation to pet initialization

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,23 @@ class Pet < ApplicationRecord
   belongs_to :owner
   has_many :veterinarians
   has_many :health_screenings
+
+  after_initialize :initialize_screenings
+
+  def initialize_screenings
+    if self.health_screenings.empty?
+      (self.cat? ? HealthScreening.cat_screenings : HealthScreening.dog_screenings).each do |name, interval|
+        self.health_screenings.build(kind: name, renewal_interval: interval, species: self.species)
+      end
+      self.save
+    end  
+  end
+
+  def cat?
+    self.species == "Cat"
+  end
+
+  def dog?
+    self.species == "Dog"
+  end
 end
