c18bd9599513404a71d15ed498fd67a67c26c1cd
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-04 16:58:16 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,18 +21,60 @@ end
 Veterinarian.create(first_name: "Dr. Joe", last_name: "Smith", email: "drjoesmith@fake.com", password: "password")
 Veterinarian.create(first_name: "Dr. Jane", last_name: "Doe", email: "drjanedoe@fake.com", password: "password")
 
-Pet.create(name: "Leo", species: "Cat", birth_date: DateTime.new(2017, 6,3), owner: Owner.find_by(first_name: "Hanna"), vet_ids: [1])
+leo = Pet.create(name: "Leo", species: "Cat", birth_date: DateTime.new(2017, 6,3), owner: Owner.find_by(first_name: "Hanna"), vet_ids: [1])
 
-Pet.create(name: "Starbuck", species: "Cat", birth_date: DateTime.new(2016, 10,25), owner: Owner.find_by(first_name: "Hank"), vet_ids: [1])
+starbuck = Pet.create(name: "Starbuck", species: "Cat", birth_date: DateTime.new(2016, 10,25), owner: Owner.find_by(first_name: "Hank"), vet_ids: [1])
 
-Pet.create(name: "Prudence", species: "Cat", birth_date: DateTime.new(2010, 8, 30), owner: Owner.find_by(first_name: "Miranda"), vet_ids: [2])
+prudence = Pet.create(name: "Prudence", species: "Cat", birth_date: DateTime.new(2010, 8, 30), owner: Owner.find_by(first_name: "Miranda"), vet_ids: [2])
 
-Pet.create(name: "Charlie", species: "Cat", birth_date: DateTime.new(2011, 4, 20), owner: Owner.find_by(first_name: "Miranda"), vet_ids: [2])
+charlie = Pet.create(name: "Charlie", species: "Cat", birth_date: DateTime.new(2011, 4, 20), owner: Owner.find_by(first_name: "Miranda"), vet_ids: [2])
 
-Pet.create(name: "Lilly", species: "Dog", birth_date: DateTime.new(2005, 5, 19), owner: Owner.find_by(first_name: "Janet"), vet_ids: [2])
+lilly = Pet.create(name: "Lilly", species: "Dog", birth_date: DateTime.new(2005, 5, 19), owner: Owner.find_by(first_name: "Janet"), vet_ids: [2])
 
-Pet.create(name: "Cooper", species: "Cat", birth_date: DateTime.new(2016, 8, 4), owner: Owner.find_by(first_name: "Travis"), vet_ids: [1])
+cooper = Pet.create(name: "Cooper", species: "Cat", birth_date: DateTime.new(2016, 8, 4), owner: Owner.find_by(first_name: "Travis"), vet_ids: [1])
 
-Pet.create(name: "Emily", species: "Dog", birth_date: DateTime.new(2012, 1, 7), owner: Owner.find_by(first_name: "Travis"), vet_ids: [1])
+emily = Pet.create(name: "Emily", species: "Dog", birth_date: DateTime.new(2012, 1, 7), owner: Owner.find_by(first_name: "Travis"), vet_ids: [1])
 
-Pet.create(name: "Mitsie", species: "Dog", birth_date: DateTime.new(2015, 6, 15), owner: Owner.find_by(first_name: "Miranda"), vet_ids: [1])
+mitsie = Pet.create(name: "Mitsie", species: "Dog", birth_date: DateTime.new(2015, 6, 15), owner: Owner.find_by(first_name: "Miranda"), vet_ids: [1])
+
+leo.health_screenings.find_by(kind: "Rabies").update(last_updated: DateTime.new(2017,10,1))
+
+leo.health_screenings.find_by(kind: "Feline Influenza").update(last_updated: DateTime.new(2017,10,1))
+
+leo.health_screenings.find_by(kind: "Stool Analysis").update(last_updated: DateTime.new(2017,10,1))
+
+cooper.health_screenings.find_by(kind: "Rabies").update(last_updated: DateTime.new(2018, 1, 20))
+
+cooper.health_screenings.find_by(kind: "Feline Influenza").update(last_updated: DateTime.new(2018, 1, 20))
+
+cooper.health_screenings.find_by(kind: "Stool Analysis").update(last_updated: DateTime.new(2018, 1, 20))
+
+starbuck.health_screenings.find_by(kind: "Rabies").update(last_updated: DateTime.new(2017,10,1))
+
+starbuck.health_screenings.find_by(kind: "Feline Leukemia").update(last_updated: DateTime.new(2017,10,1))
+
+starbuck.health_screenings.find_by(kind: "Stool Analysis").update(last_updated: DateTime.new(2017,10,1))
+
+prudence.health_screenings.each do |scr|
+  scr.update(last_updated: DateTime.new(2014, 2, 18))
+end
+
+charlie.health_screenings.each do |scr|
+  scr.update(last_updated: DateTime.new(2016, 4, 18))
+end
+
+lilly.health_screenings.find_by(kind: "Parvovirus").update(last_updated: DateTime.new(2017,10,1))
+
+lilly.health_screenings.find_by(kind: "CAV-2/Kennel Cough").update(last_updated: DateTime.new(2017,10,1))
+
+lilly.health_screenings.find_by(kind: "Stool Analysis").update(last_updated: DateTime.new(2014,10,1))
+
+lilly.health_screenings.find_by(kind: "Rabies").update(last_updated: DateTime.new(2012,10,1))
+
+emily.health_screenings.find_by(kind: "CAV-1/Canine Hepatitis").update(last_updated: DateTime.new(2016,6,14))
+
+emily.health_screenings.find_by(kind: "CAV-2/Kennel Cough").update(last_updated: DateTime.new(2016,6,14))
+
+emily.health_screenings.find_by(kind: "Lyme").update(last_updated: DateTime.new(2016,6,14))
+
+emily.health_screenings.find_by(kind: "Rabies").update(last_updated: DateTime.new(2016,6,14))
