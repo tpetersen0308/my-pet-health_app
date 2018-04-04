bcde25631b2d1e190ca930d93836dab5d9909248
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-04 15:55:12 -0700

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   def name
     self.first_name + self.last_name
   end
