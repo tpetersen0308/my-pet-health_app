b02a5db9bc895930bedeca7ec1bed93dd9782ae1
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-04 18:00:58 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :health_screenings
-  resources :pets
-  resources :users
+
+  get "login" => "sessions#new", as: "login"
+  post "login" => "sessions#create"
+  get "logout" => "sessions#destroy", as: "logout"
+
+  resources :users do 
+    resources :pets
+  end
+
+  root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
