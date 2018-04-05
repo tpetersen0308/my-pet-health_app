95b69ae8c9738dbce74092a4e94583efed5bf138
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-05 16:36:28 -0700

    add nav header helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def nav_header
+    nav_html = content_tag(:li, link_to("Home", root_path))
+    if logged_in?
+      nav_html += content_tag(:li, link_to("Log Out", logout_path))
+      if current_user.owner?
+        nav_html += content_tag(:li, link_to("My Pets", user_pets_path(current_user))) +
+                    content_tag(:li, link_to("Register a New Pet", new_user_pet_path(current_user)))
+      elsif current_user.vet?
+        nav_html += content_tag(:li, link_to("My Patients", user_pets_path(current_user)))
+      end
+    else
+      nav_html += content_tag(:li, link_to("Sign Up", new_user_path)) +
+                  content_tag(:li, link_to("Log In", login_path)) +
+                  content_tag(:li, link_to("Find a Pet", users_path))
+    end
+  end
 end
