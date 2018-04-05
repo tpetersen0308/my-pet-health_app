98b00d8489eb0da7addc9f154e6480a093c2d22c
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-04 18:47:40 -0700

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+    def current_user
+      User.find_by(:id => session[:user_id])
+    end
+
+    def logged_in?
+      session[:user_id]
+    end
+
+    def vets_only
+      if current_user.class.name != "Veterinarian"
+        flash[:alert] = "Sorry, that action is only available to veterinarians."
+        redirect_to user_path(current_user)
+      end
+    end
+
+    def owners_only
+      if current_user.class.name != "Owner"
+        flash[:alert] = "Sorry, that action is only available to pet owners."
+        redirect_to user_path(current_user)
+      end
+    end
 end
