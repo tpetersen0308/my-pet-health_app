084df5a2d65daa104528d4e9b9c3df46a264a306
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-05 18:42:34 -0700

    refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 class UsersController < ApplicationController
 
+  def index
+
+  end
+
   def new
     @user = User.new
   end
 
   def create
+    raise params.inspect
     if params[:user][:vet]
-      user = Veterinarian.new(user_params)
+      @user = Veterinarian.new(user_params)
     else
-      user = Owner.new(user_params)
+      @user = Owner.new(user_params)
     end
 
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
-      @user = User.new
       render :new
     end
   end
