4d7fd82c197d5db9257a190ab7b43c2b3703ffef
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-05 17:24:24 -0700

    add user params helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
@@ -11,4 +11,9 @@ class UsersController < ApplicationController
   def show
 
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
