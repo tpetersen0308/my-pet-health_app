223b12b4983b35a0a8369520fbb017423e06640e
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-05 17:07:35 -0700

    add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = user.find_by(:email => params[:email])
+
+    return head(:forbidden) unless user.authenticate(params[:password])
+    session[:user_id] = user.id
+
+    redirect_to user_path(user)
+  end
+
 
 
 end
