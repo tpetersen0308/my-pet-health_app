1b9ecfb80ba61e4818f450d8703ebcad301664a4
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-06 15:47:58 -0700

    build index action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,23 @@
 class PetsController < ApplicationController
 
   def index
-
+    if params[:user_id]
+      @user = User.find_by(:id => params.require(:user_id))
+      if @user
+        if @user.owner?
+          @pets = Pet.all.select{|pet| pet.owner_id == @user.id}
+        elsif @user.vet?
+          @pets = Pet.all.select{|pet| pet.veterinarian_id == @user.id}
+        end
+      else
+        flash[:alert] = "Cannot find user."
+        redirect_to root_path
+      end
+    else
+      @pets = Pet.all  
+    end 
   end
+
 
   def show
 
