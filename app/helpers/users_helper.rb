594bd0fae1c79cc7b314bf53e99c49c3ce5b1e87
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-06 15:28:57 -0700

    add pets link helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def user_pets_link(user)
+    if user.owner?
+      link_to "View #{user.name}'s pets", user_pets_path(user)
+    elsif user.vet?
+      link_to "View #{user.name}'s patients", user_pets_path(user)
+    end
+  end
+
 end
