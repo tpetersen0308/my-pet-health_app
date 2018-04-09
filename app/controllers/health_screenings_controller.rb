f678d16c69d205b0a0e86286b525783550c2593f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-09 15:08:43 -0700

    build edit action

--- app/controllers/health_screenings_controller.rb
+++ app/controllers/health_screenings_controller.rb
@@ -6,7 +6,12 @@ class HealthScreeningsController < ApplicationController
   end
 
   def edit
+    @health_screening = HealthScreening.find_by(:id => params.require(:id))
 
+    if !@health_screening || @health_screening.pet != Pet.find_by(:id => params.require(:pet_id))
+      flash[:alert] = "Invalid request"
+      redirect_to root_path
+    end
   end
 
   def update
