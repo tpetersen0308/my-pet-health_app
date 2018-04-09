3e89a0620b6094d8f39d86ff0855f599df170405
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-09 13:18:15 -0700

    add url helper for pet form

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,2 +1,10 @@
 module PetsHelper
+
+  def url_for_pet_form(pet)
+    if pet.persisted?
+      "/users/#{pet.owner.id}/pets/#{pet.id}"
+    else
+      "/users/#{params[:user_id]}/pets"
+    end
+  end
 end
