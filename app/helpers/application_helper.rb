59fb7ab06707f06e679372606c72ac3e29c35df3
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-10 12:50:31 -0700

    refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def nav_header
     nav_html = content_tag(:li, link_to("Home", root_path))
     if logged_in?
-      nav_html += content_tag(:li, link_to("Log Out", logout_path))
+      nav_html += content_tag(:li, link_to("Sign Out", logout_path))
       if current_user.owner?
         nav_html += content_tag(:li, link_to("My Pets", user_pets_path(current_user))) +
                     content_tag(:li, link_to("Register a New Pet", new_user_pet_path(current_user)))
@@ -12,7 +12,7 @@ module ApplicationHelper
       end
     else
       nav_html += content_tag(:li, link_to("Sign Up", new_user_path)) +
-                  content_tag(:li, link_to("Log In", login_path))
+                  content_tag(:li, link_to("Sign In", login_path))
                   content_tag(:li, link_to("Find a Pet", pets_path))
     end
     nav_html += content_tag(:li, link_to("Find a Pet", pets_path))
