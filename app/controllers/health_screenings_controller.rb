bcebb4b65376d279c7fea19367149110c53c981d
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-10 13:20:31 -0700

    refactor

--- app/controllers/health_screenings_controller.rb
+++ app/controllers/health_screenings_controller.rb
@@ -17,7 +17,7 @@ class HealthScreeningsController < ApplicationController
   def edit
     if !@health_screening || @health_screening.pet != Pet.find_by(:id => params.require(:pet_id))
       flash[:alert] = "Invalid request"
-      redirect_to root_path
+      redirect_to pets_path
     end
   end
 
