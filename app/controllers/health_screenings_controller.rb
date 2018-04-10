4022613d6ec607599cef782a380ee06e452acd8c
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-10 13:16:40 -0700

    refactor

--- app/controllers/health_screenings_controller.rb
+++ app/controllers/health_screenings_controller.rb
@@ -1,6 +1,7 @@
 class HealthScreeningsController < ApplicationController
   before_action :logged_in_only, only: [:edit]
   before_action :vets_only, only: [:edit]
+  before_action :set_screening, only: [:edit, :update]
 
 
   def index
@@ -9,8 +10,6 @@ class HealthScreeningsController < ApplicationController
   end
 
   def edit
-    @health_screening = HealthScreening.find_by(:id => params.require(:id))
-
     if !@health_screening || @health_screening.pet != Pet.find_by(:id => params.require(:pet_id))
       flash[:alert] = "Invalid request"
       redirect_to root_path
@@ -18,12 +17,10 @@ class HealthScreeningsController < ApplicationController
   end
 
   def update
-    health_screening = HealthScreening.find_by(:id => params.require(:id))
-    
-    if health_screening.update(screening_params)
-      health_screening.pet.veterinarians << current_user unless health_screening.pet.veterinarians.include?(current_user)
-      health_screening.pet.save
-      redirect_to pet_health_screenings_path(health_screening.pet)
+    if @health_screening.update(screening_params)
+      @health_screening.pet.veterinarians << current_user unless @health_screening.pet.veterinarians.include?(current_user)
+      @health_screening.pet.save
+      redirect_to pet_health_screenings_path(@health_screening.pet)
     else
       render :edit
     end
@@ -32,6 +29,10 @@ class HealthScreeningsController < ApplicationController
   private
   def screening_params
     params.require(:health_screening).permit(:last_updated)
+  end
+
+  def set_screening
+    @health_screening = HealthScreening.find_by(:id => params.require(:id))
   end
 
 end
