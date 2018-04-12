c7f7a33468e9d71145e33af80f623687879cbc11
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-12 12:39:09 -0700

    make e-mail authentication case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+
+  scope :ci_find, lambda { |attribute, value| where("lower(#{attribute}) = ?", value.downcase).first }
 
   def name
     "#{self.first_name} #{self.last_name}"
