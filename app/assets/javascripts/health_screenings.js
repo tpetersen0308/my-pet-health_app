31efa5c736245706556c3458db693afb2f82afe4
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-20 16:30:51 -0700

    change lastUpdated to be set as new Date object in JS HealthScreening class

--- app/assets/javascripts/health_screenings.js
+++ app/assets/javascripts/health_screenings.js
@@ -2,7 +2,7 @@ class HealthScreening {
   constructor(kind, species, lastUpdated, status) {
     this.kind = kind;
     this.species = species;
-    this.lastUpdated = lastUpdated,
+    this.lastUpdated = new Date(lastUpdated),
     this.status = status
   }
 }
