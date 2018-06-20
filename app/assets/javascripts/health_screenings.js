8edc51b87b9f09a932b02707e74b91dc27d3af94
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-20 15:33:11 -0700

    create health screening JS class

---
+++ app/assets/javascripts/health_screenings.js
@@ -0,0 +1,8 @@
+class HealthScreening {
+  constructor(kind, species, lastUpdated, status) {
+    this.kind = kind;
+    this.species = species;
+    this.lastUpdated = lastUpdated,
+    this.status = status
+  }
+}
