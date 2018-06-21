b76f8e6f2c171d02f2e51ace1d21b150f25ace1f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-21 15:35:22 -0700

    add currentUserVet() function that returns true if user is logged in as a vet

--- app/assets/javascripts/health_screenings.js
+++ app/assets/javascripts/health_screenings.js
@@ -53,6 +53,10 @@ function showScreening(screening, petId) {
   $("#js-screenings-" + petId).append(`<p>${screening.kind}</p><ul><li id="js-lastUpdated-${screening.id}">Last Updated: ${screening.displayLastUpdated()} </li><li>Status: ${screening.status}</li></ul>`);
 }
 
+function currentUserVet() {
+  $("#js-currentUserVet").val();
+}
+
 $(function() {
   attachListeners();
 })
