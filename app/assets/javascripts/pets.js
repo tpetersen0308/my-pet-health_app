70bd94ec5c328d9588a6eaf422fb2cbacdc7e249
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-22 16:04:27 -0700

    add vet info with page links to search results

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -22,9 +22,12 @@ function search() {
       $("#js-submitSearch").removeAttr('disabled');
       let newPetHTML = `<h3>Search Results: </h3>`;
       if(data){
+        debugger
         let newPet = new Pet(data.id, data.name, data.species, data.age, data.sex);
         let newOwner = new User(data.owner.id, data.owner.first_name, data.owner.last_name);
-        newPetHTML += `<h4>${newPet.name}</h4><ul><li>Type: ${newPet.sex} ${newPet.species}</li><li><a href='/users/${newOwner.id}'>${newOwner.displayFullName()}</a></li><li>Age: ${newPet.age}</li></ul>`;
+        let vets = data.veterinarians.map(vet => new User(vet.id, vet.first_name, vet.last_name));
+        let vetsHTML = "<li>Veterinarians:<ul>" + vets.map(vet => `<li><a href='users/${vet.id}'>${vet.displayFullName()}</a></li>`).join('') + "</ul></li>";
+        newPetHTML += `<h4>${newPet.name}</h4><ul><li>Type: ${newPet.sex} ${newPet.species}</li><li><a href='/users/${newOwner.id}'>${newOwner.displayFullName()}</a></li><li>Age: ${newPet.age}</li>${vetsHTML}</ul>`;
       } else {
         newPetHTML += "<p>We're sorry, we were unable to find any pets that match your search criteria.</p>";
       }
