2890dac47562d4289ea477712ac148b25ea117a1
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-22 14:54:04 -0700

    display pet search results in search results div with AJAX

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -1,8 +1,8 @@
 class Pet {
-  constructor(name, species, birthDate, sex) {
+  constructor(name, species, age, sex) {
     this.name = name;
     this.species = species;
-    this.birthDate = birthDate;
+    this.age = age;
     this.sex = sex;
   }
 }
@@ -13,7 +13,13 @@ function search() {
     let values = $(this).serialize();
     let posting = $.post($(this).attr("action"), values);
     posting.done(function(data){
-      
+      let newPet = new Pet(data.name, data.species, data.age, data.sex);
+      let newPetHTML = `<h4>${newPet.name}</h4><ul><li>Type: ${newPet.sex} ${newPet.species}</li><li>Age: ${newPet.age}</li></ul>`
+      $("#js-searchResults").html(newPetHTML);
     })
   })
 }
+
+$(function() {
+  search();
+})
