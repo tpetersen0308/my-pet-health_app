90f2d42c0d3ece05c840be91422f0f8b5132bcc9
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-22 16:15:34 -0700

    move pet html creation to displayPet() function

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -8,6 +8,15 @@ class Pet {
   }
 }
 
+function displayPet(data) {
+  let newPet = new Pet(data.id, data.name, data.species, data.age, data.sex);
+  let newOwner = new User(data.owner.id, data.owner.first_name, data.owner.last_name);
+  let vets = data.veterinarians.map(vet => new User(vet.id, vet.first_name, vet.last_name));
+  let vetsHTML = "<li>Veterinarians:<ul>" + vets.map(vet => `<li><a href='users/${vet.id}'>${vet.displayFullName()}</a></li>`).join('') + "</ul></li>";
+  let petHTML = `<h4>${newPet.name}</h4><ul><li>Type: ${newPet.sex} ${newPet.species}</li><li><a href='/users/${newOwner.id}'>${newOwner.displayFullName()}</a></li><li>Age: ${newPet.age}</li>${vetsHTML}</ul>`;
+  return petHTML;
+}
+
 function search() {
   $("#js-search").submit(function(event){
     event.preventDefault();
@@ -22,12 +31,7 @@ function search() {
       $("#js-submitSearch").removeAttr('disabled');
       let newPetHTML = `<h3>Search Results: </h3>`;
       if(data){
-        debugger
-        let newPet = new Pet(data.id, data.name, data.species, data.age, data.sex);
-        let newOwner = new User(data.owner.id, data.owner.first_name, data.owner.last_name);
-        let vets = data.veterinarians.map(vet => new User(vet.id, vet.first_name, vet.last_name));
-        let vetsHTML = "<li>Veterinarians:<ul>" + vets.map(vet => `<li><a href='users/${vet.id}'>${vet.displayFullName()}</a></li>`).join('') + "</ul></li>";
-        newPetHTML += `<h4>${newPet.name}</h4><ul><li>Type: ${newPet.sex} ${newPet.species}</li><li><a href='/users/${newOwner.id}'>${newOwner.displayFullName()}</a></li><li>Age: ${newPet.age}</li>${vetsHTML}</ul>`;
+        newPetHTML += displayPet(data);
       } else {
         newPetHTML += "<p>We're sorry, we were unable to find any pets that match your search criteria.</p>";
       }
