736585321a86e44b398c91402dd3cb571c17e15c
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-22 18:00:22 -0700

    add class and data attributes to view pets links

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
 
   def user_pets_link(user)
     if user.owner?
-      link_to "View #{user.first_name}'s pets", user_pets_path(user)
+      link_to "View #{user.first_name}'s pets", user_pets_path(user), class: 'js-viewPets', data: {id: user.id, name: user.first_name}
     elsif user.vet?
-      link_to "View #{user.first_name}'s patients", user_pets_path(user)
+      link_to "View #{user.first_name}'s patients", user_pets_path(user), class: 'js-viewPets', data: {id: user.id, name: user.first_name}
     end
   end
 
