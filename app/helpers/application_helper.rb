aba7aa8ad837c723c3d628d16853e91049e564bf
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-25 16:50:59 -0700

    add class and data id html attributes to register pets link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
       nav_html += content_tag(:li, link_to("Sign Out", logout_path))
       if current_user.owner?
         nav_html += content_tag(:li, link_to("My Pets", user_pets_path(current_user))) +
-                    content_tag(:li, link_to("Register a New Pet", new_user_pet_path(current_user)))
+                    content_tag(:li, link_to("Register a New Pet", new_user_pet_path(current_user), {class: "js-registerPet", data: {id: current_user.id}}))
       elsif current_user.vet?
         nav_html += content_tag(:li, link_to("My Patients", user_pets_path(current_user)))
       end
