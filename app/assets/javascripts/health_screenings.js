d7f95d8cf0fcce07a0cd70e29921f09e84aa04d2
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-26 17:46:05 -0700

    remove commented-out code

--- app/assets/javascripts/health_screenings.js
+++ app/assets/javascripts/health_screenings.js
@@ -20,16 +20,11 @@ function attachListeners() {
   viewScreenings();
 }
 
-//function currentUserVet() {
-//  return $("#js-currentUserVet").val() === "true";
-//}
-
 function viewScreenings() {
   $(".js-viewScreenings").on("click", function(event) {
     event.preventDefault();
     const id = $(this).data("id");
     const name = $(this).data("name");
-    //let screenings = [];
 
     $("#js-screeningsLink-" + id).html(`<a href='#' class='js-filterScreenings' data-id=${id} data-name=${name} data-status='current'>Current</a>`);
     $("#js-screeningsLink-" + id).append(`<span> | </span><a href='#' class='js-filterScreenings' data-id=${id} data-name=${name} data-status='overdue'>Overdue</a>`);
@@ -37,17 +32,6 @@ function viewScreenings() {
     $("#js-screeningsLink-" + id).append(`<span> | </span><a href='#' class='js-hideScreenings' data-id=${id} data-name=${name}>Hide</a>`);
 
     filterScreenings(`/pets/${id}/health_screenings`, id);
-    //$.getJSON(`/pets/${id}/health_screenings`, function(data){
-    //  $("#js-screenings-" + id).html(''); //clear screenings div to prevent appending duplicates
-    //  for(screening of data) {
-    //    screenings.push(new HealthScreening(screening.id, screening.kind, screening.species, screening.last_updated, screening.status));
-    //    showScreening(screenings[screenings.length - 1], id);
-    //    if(currentUserVet() && screening.status === "Overdue") {
-    //      $("#js-lastUpdated-" + screening.id).append(`<button class="js-updateScreening" data-id=${screening.id} data-pet-id=${id}>Update</button>`);
-    //      updateScreening();
-    //    }
-    //  }
-    //});
 
     $(".js-filterScreenings").on("click", function(event) {
       event.preventDefault();
@@ -86,18 +70,6 @@ function updateScreening() {
     
     req.success(function(){
       submitScreeningUpdate(screeningId);
-      //$("form").submit(function(event){
-      //  event.preventDefault();
-      //  let values = $(this).serialize();
-      //  let posting = $.post($(this).attr("action"), values);
-      //  
-      //  posting.done(function (data) {
-      //    $.getJSON(`/health_screenings/${screeningId}`, function(screeningData){
-      //      let newScreening = new HealthScreening(screeningData.id, screeningData.kind, screeningData.species, screeningData.last_updated, screeningData.status);
-      //      $("#js-screening-" + screeningId).html(`<p>${newScreening.kind}</p><ul><li id="js-lastUpdated-${newScreening.id}">Last Updated: ${newScreening.displayLastUpdated()} </li><li>Status: ${newScreening.status}</li></ul>`);
-      //    })
-      //  })
-      //})
     })
   })
 }
