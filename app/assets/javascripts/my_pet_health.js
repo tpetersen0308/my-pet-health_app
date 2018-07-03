4c3812207c9fc70b284726f43dfa3d384d7446f5
tpetersen0308 <travis.e.petersen@gmail.com>
2018-07-03 11:55:24 -0700

    refactor cancel() to have an optional id arg such that form cancel logic is different for new vs edit pet

--- app/assets/javascripts/my_pet_health.js
+++ app/assets/javascripts/my_pet_health.js
@@ -1,6 +1,15 @@
-function cancel() {
-  $(".js-cancel").on("click", function(event) {
-    event.preventDefault();
-    $("#js-content").html("");
-  })
+function cancel(id = nil) {
+  if(id){
+    $("#js-cancel-" + id).on("click", function(event) {
+      event.preventDefault();
+      $.getJSON("/pets/" + id, function(data) {
+        $("#js-pet-" + id).html(displayPet(data));
+      })
+    })
+  } else {
+    $(".js-cancel").on("click", function(event) {
+      event.preventDefault();
+      $("#js-content").html("");
+    })
+  }
 }
